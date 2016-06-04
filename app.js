118a536a278afc7088db76d4e692e699ee3a7c53
Misha <meshuin94@gmail.com>
2016-06-04 23:15:52 +0400

    add db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express  = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var path = require('path');
+var config = require('./config');
  
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
@@ -14,20 +15,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('Video'));
 app.use(express.static('uploads'));
 require('./app/routes.js')(app, http);
-
-http.listen(8090);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+http.listen(config.get('port'));
 
  
 
