b3f18321b4286506d31b4d3037d7505c88978d9c
Misha <meshuin94@gmail.com>
2016-06-08 14:30:46 +0400

    add some actions and test to socket io

--- app/socket.js
+++ app/socket.js
@@ -3,27 +3,29 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var util = require('util');
 var Files = {};
+var GameModel = require('../models/Game').Game;
 
 var ffmpeg = require('fluent-ffmpeg');
 var numUsers = 0;
 var dataGame =  require('./DataGame').DataGame;
-module.exports = function(http){
-	var io = socketIO.listen(http);
    	function Game(idRoom){
 		this.isActive = true;
 		this.idRoom = idRoom;
+		this.error = null;
+		this.gameModel;	
 		this.masterSocket = null;
 		this.sockets = [];
 	}
-	Game.prototype.add = function(socket, isLead){
+	Game.prototype.add = function(socket, isLead, gameModel){
 		if (isLead == true){
 			this.masterSocket = socket;
+			this.gameModel = gameModel;
 		}else {
 			this.sockets.push(socket);
 		}
 	}
 	var games = [];
-	function getRoomById(idRoom){
+	function getGameById(idRoom){
 		for (var i = 0; i < games.length; ++i){
 			if (games[i].idRoom == idRoom){
 				return games[i];
@@ -34,25 +36,46 @@ module.exports = function(http){
 		return games[lastIndex - 1];
 	}
 	
-	function addInCurrentOrCreateRoom(idRoom, socket, isLead){
-		var room = getRoomById(idRoom);
-		//console.log(room);
-
-		room.add(socket, isLead);
-		//console.log(room);
+	function addInCurrentOrCreateRoom(idRoom, socket, isLead, gameModel){
+		var game = getGameById(idRoom);
+		game.add(socket, isLead, gameModel);
 		console.log(games.length);
 	} 
+module.exports = function(http){
+	var io = socketIO.listen(http);
 	io.sockets.on('connection', function (socket) {
 		console.log("connection");
 		 socket.on('handshake',function(user){
    			socket.join(user.id_room);
 			socket.room = user.id_room;
 			console.log(socket.room + "   " + user.vk_id);
-			addInCurrentOrCreateRoom(user.id_room, socket, user.is_lead);
+			console.log(user.game);
+			addInCurrentOrCreateRoom(socket.room, socket, user.is_lead, user.game);
+		
 			io.to(socket.room).emit('new user', user.vk_id + " has joined.");
 		 })
-
 		
+		socket.on('start_game', function(data){
+			var game = getGameById(socket.room);
+			var data = {};
+			data.file = game.gameModel.file.path;
+			data.message = "start game";
+			io.to(socket.room).emit('on_start_game',data);
+		});
+
+		socket.on('word', function(word){
+			var game = getGameById(socket.room);
+			var wordLowerCase = game.gameModel.word.toLowerCase(); 
+			var prevWordLowerCase =word.toLowerCase();
+			if (wordLowerCase == prevWordLowerCase){
+				io.to(socket.room).emit('win', word);
+			}else {
+				io.to(socket.room).emit('word', word);
+			}
+			if (game.masterSocket == null){
+				game.masterSocket.emit('estimate');
+			}	
+		})
 	});
 	
 	return io;
